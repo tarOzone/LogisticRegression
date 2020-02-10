59d07dc35b326f882f69b94cb6daedf24e22559b
tarOzone <tarthewongkham0@gmail.com>
2020-02-11 00:54:13 +0700

    Second commit

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
-
+from sklearn.metrics import *
 from utils.utils import *
 
 
